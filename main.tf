cbb6b8efd7c6c82b56ca73bcae32fb502135c025
Martin Alexander <malexander@gogoair.com>
2019-01-21 16:45:04 -0600

    Added public/prvt/rds security groups

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
   profile = "${var.aws_profile}"
 }
 
-#----- IAM -----
+# ----- IAM -----
 
-#S3_access
+# S3_access
 
 resource "aws_iam_instance_profile" "s3_access_profile" {
   name = "s3_access"
@@ -62,7 +62,7 @@ resource "aws_vpc" "wp_vpc" {
   }
 }
 
-#Internet Gateway
+# Internet Gateway
 
 resource "aws_internet_gateway" "wp_internet_gateway" {
   vpc_id = "${aws_vpc.wp_vpc.id}"
@@ -72,7 +72,7 @@ resource "aws_internet_gateway" "wp_internet_gateway" {
   }
 }
 
-#Route Tables
+# Route Tables
 
 resource "aws_route_table" "wp_public_rt" {
   vpc_id = "${aws_vpc.wp_vpc.id}"
@@ -95,7 +95,7 @@ resource "aws_default_route_table" "wp_private_rt" {
   }
 }
 
-#Subnets
+# Subnets
 
 resource "aws_subnet" "wp_public1_subnet" {
   vpc_id                  = "${aws_vpc.wp_vpc.id}"
@@ -174,7 +174,7 @@ resource "aws_subnet" "wp_rds3_subnet" {
   }
 }
 
-#RDS Subnet Group
+# RDS Subnet Group
 
 resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
   name = "wp_rds_subnetgroup"
@@ -188,7 +188,7 @@ resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
   }
 }
 
-#Subnet Associations
+# Subnet Associations
 
 resource "aws_route_table_association" "wp_public1_assoc" {
   subnet_id= "${aws_subnet.wp_public1_subnet.id}"
@@ -209,6 +209,114 @@ resource "aws_route_table_association" "wp_private2_assoc" {
   subnet_id = "${aws_subnet.wp_private2_subnet.id}"
   route_table_id = "${aws_default_route_table.wp_private_rt.id}"
 }
+
+# Security Groups
+
+resource "aws_security_group" "wp_dev_sg" {
+  name = "wp_dev_sg"
+  description = "Used for access to the dev instance"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  #SSH
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  #HTTP
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Public Security Group
+
+resource "aws_security_group" "wp_public_sg" {
+  name = "wp_public_sg"
+  description = "Used for the elastic load balancer for public access"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  #HTTP
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/32"]
+  }
+}
+
+# Private Security Group
+
+resource "aws_security_group" "wp_private_sg" {
+  name = "wp_private_sg"
+  description = "used for private instances"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # Access from VPC
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# RDS Security Group
+
+resource "aws_security_group" "wp_rds_sg" {
+  name = "wp_rds_sg"
+  description = "Used for RDS instances"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # SQL Access from public/private security groups
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+
+    security_groups = ["${aws_security_group.wp_dev_sg.id}",
+    "${aws_security_group.wp_public_sg.id}",
+    "${aws_security_group.wp_private_sg.id}",
+    ]
+  }
+}
+
+
+
+
+
+
 
 
 
