16254fe5fef312c0553e3f1925d4f4da826f7210
Martin Alexander <malexander@gogoair.com>
2019-01-30 16:16:17 -0600

    adding outputs
    Signed-off-by: Martin Alexander <malexander@gogoair.com>

--- manage_apps_and_infra_course/main.tf
+++ manage_apps_and_infra_course/main.tf
@@ -14,5 +14,14 @@ resource "docker_container" "container_id" {
     internal = "2368"
     external = "80"
   }
+}
 
+# Output name and ip address
+
+output "ip address" {
+  value = "${docker_container.container_id.ip_address}"
+}
+
+output "container_name" {
+  value = "${docker_container.container_id.name}"
 }
