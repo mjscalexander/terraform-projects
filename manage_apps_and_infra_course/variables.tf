e3850efe07d02d7c91d25b22a2a9a9641279b6c3
Martin Alexander <malexander@gogoair.com>
2019-02-04 08:03:33 -0600

    added terraform.tfvars file
    Signed-off-by: Martin Alexander <malexander@gogoair.com>

--- manage_apps_and_infra_course/variables.tf
+++ manage_apps_and_infra_course/variables.tf
@@ -5,34 +5,18 @@ variable "env" {
 variable "image" {
   description = "image for container"
   type = "map"
-  default = {
-    dev = "ghost:latest"
-    prod = "ghost:alpine"
-  }
 }
 variable "container_name" {
   description = "name of container"
   type = "map"
-  default = {
-    dev = "dev_blog"
-    prod = "prod_blog"
-  }
 }
 
 variable "int_port" {
   description = "internal port for container"
   type = "map"
-  default = {
-    dev = "2368"
-    prod = "2368"
-  }
 }
 
 variable "ext_port" {
   description = "external port for container"
   type = "map"
-  default = {
-    dev = "8080"
-    prod = "80"
-  }
 }
