1d55e822ef5df43c067f5709670a5b1d3b8a1e8c
Martin Alexander <malexander@gogoair.com>
2019-02-06 16:16:58 -0600

    moving all files for docker resouces to docker folder
    Signed-off-by: Martin Alexander <malexander@gogoair.com>

--- manage_apps_and_infra_course/container/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output the IP Address and name of container
-
-output "ip" {
-  value = "${docker_container.container_id.ip_address}"
-}
-
-output "container_name" {
-  value = "${docker_container.container_id.name}"
-}
--- manage_apps_and_infra_course/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output name and ip address
-
-output "ip address" {
-  value = "${module.container.ip}"
-}
-
-output "container_name" {
-  value = "${module.container.container_name}"
-}
