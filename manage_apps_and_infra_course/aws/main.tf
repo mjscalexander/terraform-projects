fe020f765a78d10615c8d3c3ef4466b24565105e
Martin Alexander <malexander@gogoair.com>
2019-02-12 08:51:22 -0600

    lots of changes
    Signed-off-by: Martin Alexander <malexander@gogoair.com>

--- manage_apps_and_infra_course/aws/main.tf
+++ manage_apps_and_infra_course/aws/main.tf
@@ -9,3 +9,10 @@ module "storage" {
   source = "./storage"
   project_name = "${var.project_name}"
 }
+# Deploy networking resource
+module "networking" {
+  source = "./networking"
+  vpc_cidr = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  accessip = "${var.accessip}"
+}
--- manage_apps_and_infra_course/aws/variables.tf
+++ manage_apps_and_infra_course/aws/variables.tf
@@ -2,3 +2,8 @@
 
 variable "aws_region" {}
 variable "project_name" {}
+variable "vpc_cidr" {}
+variable "public_cidrs" {
+  type = "list"
+}
+variable "accessip" {}
